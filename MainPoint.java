b4f0476176a237c18dd090da7f9f748ad8776012
KirillPamPam <kirill.mai308@gmaio.com>
2015-07-17 12:18:34 +0400

    Create MainPoint.java

---
+++ MainPoint.java
@@ -0,0 +1,14 @@
+public class MainPoint {
+    private static Point point = new Point();
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        CreateImage image = new CreateImage(1000, 600, point);
+        System.out.print("Radius = ");
+        int radius = sc.nextInt();
+        point.findPointsAndCenter(radius);
+
+        image.drawImage(radius);
+    }
+
+}
