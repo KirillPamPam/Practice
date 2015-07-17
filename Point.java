6ed0fa149fb3462878d630a818a5c497ccbd1c25
KirillPamPam <kirill.mai308@gmaio.com>
2015-07-17 12:51:52 +0400

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class Point {
     private ArrayList<PointPosition> positions = new ArrayList<>();
     private InputStream is = MainPoint.class.getResourceAsStream("/coordinates.txt");
